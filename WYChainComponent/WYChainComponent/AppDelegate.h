1b0bbd71efab73d09acfde030e612fab0ca128ff
Luis <wangyang@izhijia.com>
2020-04-23 19:01:47 +0800

    创建工程

---
+++ WYChainComponent/WYChainComponent/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WYChainComponent
+//
+//  Created by Luis on 2020/4/23.
+//  Copyright © 2020 com.Luis.mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
